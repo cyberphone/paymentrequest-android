dca8916b2acb607eca3e74bba7e05a8fc4b0110a
Anders Rundgren <anders.rundgren.net@gmail.com>
2019-08-09 10:27:29 +0200

    Elementary demo running on Android 7 and forward

--- app/src/main/java/org/webpki/androidpay/PaymentActivity.java
+++ app/src/main/java/org/webpki/androidpay/PaymentActivity.java
@@ -1,10 +1,12 @@
 package org.webpki.androidpay;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.View;
 
 public class PaymentActivity extends AppCompatActivity {
@@ -15,13 +17,23 @@ public class PaymentActivity extends AppCompatActivity {
         setContentView(R.layout.activity_payment);
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        Intent intent = getIntent();
+        Bundle extras = intent.getExtras();
+        for (String s : extras.keySet()) {
+            Log.i("KKK", s);
+        }
 
         FloatingActionButton fab = findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent result = new Intent();
+                Bundle extras = new Bundle();
+                extras.putString("methodName", "https://192.168.1.79:8442/w3cpay/method");
+                extras.putString("details", "{\"big\": \"awesome\"}");
+                result.putExtras(extras);
+                setResult(RESULT_OK, result); // Change to RESULT_CANCELED on failure.
+                finish();
             }
         });
     }
